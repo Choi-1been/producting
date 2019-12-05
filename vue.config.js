276a6faba4ea8aa49eb3ca57560be6e43868a6ad
최원빈 <chlsnjsqls730@gmail.com>
2019-12-05 22:46:59 +0900

    change index meta tag

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,5 @@ module.exports = {
     "vuetify"
   ],
   publicPath: process.env.NODE_ENV === 'production' ? '/producting/' : '/',
-  chainWebpack: (config) => {
-    config
-        .plugin('html')
-        .tap(args => {
-          args[0].title = 'CRvending';
-          args[0].meta = {viewport: 'width=1920px,height=1200px,initial-scale=1'};
-
-          return args;
-        })
-  }
 }
 
